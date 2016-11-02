322ef94c5cfd5c7632dce52c29955a9d9c215948
hadbabits <zacmantooth@gmail.com>
2016-11-02 13:17:22 -0500

    Starting on health system

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+	private int health; //Needs to be set by gameObject attached to
+
+
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		
+	}
+}
