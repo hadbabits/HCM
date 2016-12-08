cb08a457484f562e9e959e757a74cbf9c69cc85a
hadbabits <zacmantooth@gmail.com>
2016-12-08 13:35:48 -0600

    Adding a dagger(?) to player character. Animation to possibly follow.

---
+++ Assets/Scripts/DaggerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaggerScript : MonoBehaviour {
+
+	private bool damaging;
+	private Rigidbody2D daggerRb;
+	private Rigidbody2D playerRb;
+
+	void Start () {
+		damaging = false;
+		daggerRb = GetComponent<Rigidbody2D> ();
+		playerRb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetMouseButton (1)) {
+			Debug.Log ("Dag!");
+			daggerRb.position.x += 50;
+		}
+	}
+}
